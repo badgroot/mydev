d3994d9fc842e0c9390e1e96e8f05c31a4f925c6
Himanshu <hpachauri786@gmail.com>
2018-06-08 14:41:36 +0530

    adding ftp

---
+++ ftp/build/CMakeFiles/ftp_server.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ftp_server.dir/server.c.o"
+  "ftp_server.pdb"
+  "ftp_server"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ftp_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
