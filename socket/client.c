7567e68ccb0a6213585c8c419234658c8203e3cd
HIMANSHU <hpachauri786@gmail.com>
2018-10-16 10:56:41 +0530

    client server update

--- socket/client.c
+++ socket/client.c
@@ -1,43 +1,63 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#include <unistd.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <netinet/in.h>
-#define PORT 8081
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <arpa/inet.h> 
 
-
-int main(int argc,char *argv[]){
-
-
-	struct sockaddr_in address;
-	int sock=0,valread;
-	struct sockaddr_in serv_addr;
-	char *hello = "hello from client";
-	char buffer[1024];
-	if((sock=socket(AF_INET,SOCK_STREAM,0))<0)
-	{
-		perror("socket creation failed");
-		exit(EXIT_FAILURE);
-	}
-	memset(buffer,0,sizeof(buffer));
-	memset(&serv_addr,'0',sizeof(serv_addr));
-	serv_addr.sin_family=AF_INET;
-	serv_addr.sin_port=htons(PORT);
-	if(inet_pton(AF_INET,"72.142.168.226",&serv_addr.sin_addr)<=0){
-		printf("\n invalid address /  Address not support\n");
-		return -1;
-	}
-
-if(connect(sock, (const struct sockaddr *)&serv_addr, sizeof(serv_addr))<0)
+int main(int argc, char *argv[])
 {
-printf("connection failed\n");
-return -1;	
-}
-send(sock,hello,strlen(hello),0);
-printf("hello message sent\n");
-valread=read(sock,buffer,1024);
-printf("%s\n",buffer );
-return 0;
+    int sockfd = 0, n = 0;
+    char recvBuff[1024];
+    struct sockaddr_in serv_addr; 
+
+    if(argc != 2)
+    {
+        printf("\n Usage: %s <ip of server> \n",argv[0]);
+        return 1;
+    } 
+
+    memset(recvBuff, '0',sizeof(recvBuff));
+    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("\n Error : Could not create socket \n");
+        return 1;
+    } 
+
+    memset(&serv_addr, '0', sizeof(serv_addr)); 
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(5000); 
+
+    if(inet_pton(AF_INET, argv[1], &serv_addr.sin_addr)<=0)
+    {
+        printf("\n inet_pton error occured\n");
+        return 1;
+    } 
+
+    if( connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+       printf("\n Error : Connect Failed \n");
+       return 1;
+    } 
+
+    while ( (n = read(sockfd, recvBuff, sizeof(recvBuff)-1)) > 0)
+    {
+        recvBuff[n] = 0;
+        if(fputs(recvBuff, stdout) == EOF)
+        {
+            printf("\n Error : Fputs error\n");
+        }
+    } 
+
+    if(n < 0)
+    {
+        printf("\n Read error \n");
+    } 
+
+    return 0;
 }
--- socket/server.c
+++ socket/server.c
@@ -1,56 +1,43 @@
-/*
-gcc server.c -o server
-*/
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-
-#include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <time.h> 
 
-#define PORT 8080
-
-int main(int argc,char *argv[]){
-int socket_fd,new_socket,valread;
-struct  sockaddr_in address;
-int opt=1;
-int addrlen=sizeof(address);
-char buffer[1024]={0};
-char *hello="Hello from server";
-if((socket_fd=socket(AF_INET,SOCK_STREAM,0) )==0)
+int main(int argc, char *argv[])
 {
-	perror("socket failed\n");
-	exit(EXIT_FAILURE);
-}
+    int listenfd = 0, connfd = 0;
+    struct sockaddr_in serv_addr; 
 
-if(setsockopt(socket_fd, SOL_SOCKET,SO_REUSEADDR|SO_REUSEPORT,&opt, sizeof(opt))){
-	perror("setsockopt failure");
-	exit(EXIT_FAILURE);
-}
-address.sin_family=AF_INET;
-address.sin_port=htons(PORT);
-if(bind(socket_fd,(struct sockaddr *) &address, addrlen)){
-	perror("bind failure");
-	exit(EXIT_FAILURE);
-}
-if(listen(socket_fd, 3)<0)
-{
-perror("listned failure()");
-exit(EXIT_FAILURE);
+    char sendBuff[1025];
+    time_t ticks; 
 
-}
- if ((new_socket = accept(socket_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0){
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    memset(&serv_addr, '0', sizeof(serv_addr));
+    memset(sendBuff, '0', sizeof(sendBuff)); 
 
-perror("accept failed");
-exit(EXIT_FAILURE);
-}
-valread=read(new_socket,buffer,1024);
-printf("%s\n",buffer );
-send(new_socket,hello,strlen(hello),0);
-printf("Hello Message Sent to client\n");
-return 0;
-}
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(5000); 
 
+    bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)); 
+
+    listen(listenfd, 10); 
+
+    while(1)
+    {
+        connfd = accept(listenfd, (struct sockaddr*)NULL, NULL); 
+
+        ticks = time(NULL);
+        snprintf(sendBuff, sizeof(sendBuff), "Himanshu %.24s\r\n", ctime(&ticks));
+        write(connfd, sendBuff, strlen(sendBuff)); 
+
+        close(connfd);
+        sleep(1);
+     }
+}
